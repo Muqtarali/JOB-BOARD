45967791786e7810f0712b513a0bf96b927f62a3
Muqtarali <162393700+Muqtarali@users.noreply.github.com>
2025-05-21 11:17:25 +0530

    Add files via upload

---
+++ src/component/navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+    let name ="Ali";
+    return (
+        <>
+        <div className="navbar">
+            <h1>Job Board</h1>
+            </div>
+    
+        </>
+    );
+}
+
+export default Navbar;
