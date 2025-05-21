aa91bd014716420acab2ff210ab540b51dd5fe90
Muqtarali <162393700+Muqtarali@users.noreply.github.com>
2025-05-21 22:06:49 +0530

    Day 2: Removed Boilerplate and Implemented Global Styles

---
+++ src/component/navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() {
+    
+    return (
+        <>
+        <div className="navbar">
+            <h1>Job Board</h1>
+              <h1>Job Board</h1>
+            
+            </div>
+    
+        </>
+    );
+}
+
+export default Navbar;
